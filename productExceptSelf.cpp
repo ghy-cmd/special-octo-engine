3dd26a831a676e72a5aa0ce4f3bc9eeab868ff7f
ghy-cmd <guihaoyuebuaa@163.com>
2024-05-18 20:56:27 +0800

    hard

--- productExceptSelf.cpp
+++ productExceptSelf.cpp
@@ -21,9 +21,34 @@ public:
         }
         return res;
     }
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size();
+        int i = 0;
+        while (i < n) {
+            int sumOfGas = 0, sumOfCost = 0;
+            int cnt = 0;
+            while (cnt < n) {
+                int j = (i + cnt) % n;
+                sumOfGas += gas[j];
+                sumOfCost += cost[j];
+                if (sumOfCost > sumOfGas) {
+                    break;
+                }
+                cnt++;
+            }
+            if (cnt == n) {
+                return i;
+            } else {
+                i = i + cnt + 1;
+            }
+        }
+        return -1;
+    }
 };
 
 int main(){
-
+    Solution a;
+    vector<int> gas={5,8,2,8},cost={6,5,6,6};
+    cout<<a.canCompleteCircuit(gas,cost);
     return 0;
 }
