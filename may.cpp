c90861fc1e7254edd0b1d611efa3acb2803a4433
ghy-cmd <guihaoyuebuaa@163.com>
2024-05-20 18:36:21 +0800

    longestAwesome

--- may.cpp
+++ may.cpp
@@ -217,13 +217,50 @@ public:
                 matrix[0][i] = 0;
             }
         }
-        if (b)
+        if (b == 0)
         {
             for (int i = 0; i < m; i++)
             {
                 matrix[i][0] = 0;
             }
         }
+    }
+    /*
+    给你一个字符串 s 。请返回 s 中最长的 超赞子字符串 的长度。
+    「超赞子字符串」需满足满足下述两个条件：
+    该字符串是 s 的一个非空子字符串
+    进行任意次数的字符交换后，该字符串可以变成一个回文字符串
+    trick:这个数组显然是只不能遍历两次的，肯定超时。前缀和的思路也比较明显。
+    注意到从0开始的一个子串的超赞一共有2^10种状态，状态相同的子串s[0:i]和s[0:j]，可以凑出超赞的s[i:j]（只有一位状态不同的也可以）。
+    由于只用保存最长的超赞子串，所以同样状态的s[0:i] i 越小越好。
+    所以用大小为1024的字典存每个状态的最小位置就行了，这样就可以在一次遍历中找到每一个位置的最长答案。
+    */
+    int longestAwesome(string s)
+    {
+        int n = s.length(), mask = 0, res = 0;
+        vector<int> dist(1024, -1);
+        for (int i = 0; i < n; i++)
+        {
+            mask ^= 1 << (s[i] - '0');
+            if (mask == 0 || dist[mask] != -1)
+            {
+                res = max(res, i - dist[mask]);
+            }
+            else
+            {
+                dist[mask] = i;
+            }
+            int temp;
+            for (int j = 0; j < 10; j++)
+            {
+                temp = mask ^ (1 << (j));
+                if (temp == 0 || dist[temp] != -1)
+                {
+                    res = max(res, i - dist[temp]);
+                }
+            }
+        }
+        return res;
     }
 };
 
@@ -232,18 +269,18 @@ int main()
     Solution a;
     // vector<int> vec={0,1,0,2,1,0,1,3,2,1,2,1};
     // cout<<a.trap(vec)<<endl;
-    // string s = "MCMXCIV";
-    // cout << a.romanToInt(s) << endl;
-    vector<vector<int>> vec = {{1, 2, 3, 4}, {5, 0, 7, 8}, {0, 10, 11, 12}, {13, 14, 15, 0}};
-    a.setZeroes(vec);
-    for (int i = 0; i < vec.size(); i++)
-    {
-        for (int j = 0; j < vec[0].size(); j++)
-        {
-            cout << vec[i][j] << " ";
-        }
-        cout << endl;
-    }
+    string s = "1727";
+    cout << a.longestAwesome(s) << endl;
+    // vector<vector<int>> vec = {{1, 2, 3, 4}, {5, 0, 7, 8}, {0, 10, 11, 12}, {13, 14, 15, 0}};
+    // a.setZeroes(vec);
+    // for (int i = 0; i < vec.size(); i++)
+    // {
+    //     for (int j = 0; j < vec[0].size(); j++)
+    //     {
+    //         cout << vec[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
 
     return 0;
 }
