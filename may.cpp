e686e4de7013d053c16bc7e5675546e186bfac9f
ghy-cmd <guihaoyuebuaa@163.com>
2024-05-20 10:32:13 +0800

    trap

--- may.cpp
+++ may.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+class Solution {
+public:
 /*
 n 个孩子站成一排。给你一个整数数组 ratings 表示每个孩子的评分。
 
@@ -10,8 +12,6 @@ n 个孩子站成一排。给你一个整数数组 ratings 表示每个孩子的
 相邻两个孩子评分更高的孩子会获得更多的糖果。
 请你给每个孩子分发糖果，计算并返回需要准备的 最少糖果数目 。
 */
-class Solution {
-public:
     int candy(vector<int>& ratings) {
         int n=ratings.size(),res=0;
         vector<int> ans(n,1);
@@ -36,11 +36,45 @@ public:
         }
         return res;
     }
+/*
+给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+*/
+    int trap(vector<int>& height) {
+        int maxi=height[0],count=height.size(),label=0;
+        vector<int> res(count,0);
+        res[0]=maxi;
+        for (int i = 1; i < count; i++)
+        {
+            if (height[i]>=maxi)
+            {
+                for (int j = label+1; j <i; j++)
+                {
+                    res[j]=maxi;
+                }
+                maxi=height[i];
+                label=i;
+                res[i]=height[i];
+            }else
+            {
+                for (int j = label+1; j < i; j++)
+                {
+                    res[j]=max(res[j],height[i]);
+                }
+                res[i]=height[i];
+            }
+        }
+        label=0;
+        for (int i = 0; i < count; i++)
+        {
+            label+=res[i]-height[i];
+        }
+        return label;
+    }
 };
 
 int main(){
     Solution a;
-    vector<int> vec={1,3,4,5,2};
-    cout<<a.candy(vec)<<endl;
+    vector<int> vec={0,1,0,2,1,0,1,3,2,1,2,1};
+    cout<<a.trap(vec)<<endl;
     return 0;
 }
